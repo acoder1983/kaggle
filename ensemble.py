77e50dba7098a37ec33f0b15b33348244a418849
acoder1983 <acoder1983@163.com>
2017-08-02 15:43:11 +0800

    Add files via upload

--- ensemble.py
+++ ensemble.py
@@ -12,7 +12,7 @@ class EnsembleStackClassifierEx(EnsembleStackClassifier):
         EnsembleStackClassifier.__init__(self, stack,combiner)
         
     def score(self, X, y, sample_weight=None):
-        return (self.predict(X)-y).astype(float).sum()/len(X)
+        return (self.predict(X)==y).astype(float).sum()/len(X)
     
     def get_params(self, deep=True):
         return {'stack':self.stack}
